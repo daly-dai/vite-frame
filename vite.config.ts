aeab06c9d195eb11f43cade34fe519caa1405c84
dalydai <2508291029@qq.com>
2022-09-18 20:19:28 +0800

    feat: 新增代码格式化与提交约束相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig, normalizePath } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import autoprefixer from "autoprefixer"; // 自动添加css兼容前缀
-import cssnano from "cssnano"; // 压缩css体积
-import windi from "vite-plugin-windicss";
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import autoprefixer from 'autoprefixer'; // 自动添加css兼容前缀
+import cssnano from 'cssnano'; // 压缩css体积
+import windi from 'vite-plugin-windicss';
+
+import viteEslint from 'vite-plugin-eslint';
 
 // 定义css的全局变量
 const variablePath = normalizePath(
-  path.resolve("./src/assets/css/variable.scss")
+  path.resolve('./src/assets/css/variable.scss')
 );
 
 // https://vitejs.dev/config/
@@ -20,11 +22,12 @@ export default defineConfig({
         // 当然，通过这个配置你也可以添加其它的 Babel 插件
         plugins: [
           // 适配 styled-component
-          "babel-plugin-styled-components",
-        ],
-      },
+          'babel-plugin-styled-components'
+        ]
+      }
     }),
     windi(),
+    viteEslint()
   ],
   css: {
     // 进行 PostCSS 配置
@@ -32,21 +35,21 @@ export default defineConfig({
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
         }),
-        cssnano({ preset: "default" }),
-      ],
+        cssnano({ preset: 'default' })
+      ]
     },
     preprocessorOptions: {
       modules: {
         // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
         // 其中，name 表示当前文件名，local 表示类名
-        generateScopedName: "[name]__[local]___[hash:base64:5]",
+        generateScopedName: '[name]__[local]___[hash:base64:5]'
       },
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`,
-      },
-    },
-  },
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
 });
