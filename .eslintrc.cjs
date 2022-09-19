d3eb6ab4bd3072d07f4d79914c1921e86bc9c24e
dalydai <2508291029@qq.com>
2022-09-19 15:37:11 +0800

    feat: 新增静态资源的相关配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,43 +4,44 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  "settings": {
-    "react": {
+  settings: {
+    react: {
       /**
-      * "detect" automatically picks the version you have installed.
-      * You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-      * default to latest and warns if missing 
-      */
-      
-      "version": "999.999.999"   // It will default to "detect" in the future
-      
+       * "detect" automatically picks the version you have installed.
+       * You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+       * default to latest and warns if missing
+       */
+
+      version: '999.999.999' // It will default to "detect" in the future
+
       // "version": "detect"
       // "version": "16.12.0"
     }
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     // 1. 接入 prettier 的规则
-    "prettier",
-    "plugin:prettier/recommended"
+    'prettier',
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   // 2. 加入 prettier 的 eslint 插件
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     // 3. 注意要加上这一句，开启 prettier 自动修复的功能
-    "prettier/prettier": "error",
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "react/react-in-jsx-scope": "off"
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': [0, 'error', 'windows']
   }
 };
